fe2f0007c77b19592e4ce13e4feae96975b490a4
Clebson Moura <clebson.sh@gmail.com>
2024-12-07 00:05:48 -0300

    refactor install.sh for improved readability and consistency

--- install.sh
+++ install.sh
@@ -3,23 +3,23 @@
 echo "Installing Hard..."
 
 # check if git is installed
-if ! command -v git > /dev/null; then
-    echo "Git is not installed. Please, install git and try again."
-    exit 1
+if ! command -v git >/dev/null; then
+  echo "Git is not installed. Please, install git and try again."
+  exit 1
 fi
 
 # check if docker is installed
-if ! command -v docker > /dev/null; then
-    echo "Docker is not installed. Please, install docker and try again."
-    exit 1
+if ! command -v docker >/dev/null; then
+  echo "Docker is not installed. Please, install docker and try again."
+  exit 1
 fi
 
 # check if docker-compose or docker compose is installed
-if ! command -v docker-compose > /dev/null; then
-    if ! command -v docker compose > /dev/null; then
-        echo "Docker Compose is not installed. Please, install docker-compose and try again."
-        exit 1
-    fi
+if ! command -v docker-compose >/dev/null; then
+  if ! command -v docker compose >/dev/null; then
+    echo "Docker Compose is not installed. Please, install docker-compose and try again."
+    exit 1
+  fi
 fi
 
 # set HARD_PATH
@@ -27,7 +27,7 @@ HARD_PATH="/home/${USER}/.hard"
 
 # check if HARD_PATH exists
 if [ -d $HARD_PATH ]; then
-    rm -rf $HARD_PATH
+  rm -rf $HARD_PATH
 fi
 
 # clone hard repository
@@ -36,42 +36,45 @@ git clone https://github.com/clebsonsh/hard.git $HARD_PATH
 # copy .env.example to .env
 cp $HARD_PATH/.env.example $HARD_PATH/.env
 
+# source .env
 . $HARD_PATH/.env
+
+echo "If you want to change the default values, please, edit the .env file in $HARD_PATH."
 
 # check if WWW_PATH exists and create it
 if [ ! -d $WWW_PATH ]; then
-    mkdir -p $WWW_PATH
+  mkdir -p $WWW_PATH
 fi
 
 # check if .bashrc exists and have ~/.local/bin in the PATH
 if [ -f ~/.bashrc ]; then
-    if ! grep -q ".local/bin" ~/.bashrc; then
-        echo "export PATH=\$PATH:~/.local/bin" >> ~/.bashrc
-    fi
+  if ! grep -q ".local/bin" ~/.bashrc; then
+    echo "export PATH=\$PATH:~/.local/bin" >>~/.bashrc
+  fi
 fi
 
 # check if .zshrc exists and have ~/.local/bin in the PATH
 if [ -f ~/.zshrc ]; then
-    if ! grep -q ".local/bin" ~/.zshrc; then
-        echo "export PATH=\$PATH:~/.local/bin" >> ~/.zshrc
-    fi
+  if ! grep -q ".local/bin" ~/.zshrc; then
+    echo "export PATH=\$PATH:~/.local/bin" >>~/.zshrc
+  fi
 fi
 
 # check if config.fish exists and have ~/.local/bin in the PATH
 if [ -f ~/.config/fish/config.fish ]; then
-    if ! grep -q ".local/bin" ~/.config/fish/config.fish; then
-        echo "set -x PATH \$PATH ~/.local/bin" >> ~/.config/fish/config.fish
-    fi
+  if ! grep -q ".local/bin" ~/.config/fish/config.fish; then
+    echo "set -x PATH \$PATH ~/.local/bin" >>~/.config/fish/config.fish
+  fi
 fi
 
 # check if ~/.local/bin exists
 if [ ! -d ~/.local/bin ]; then
-    mkdir -p ~/.local/bin
+  mkdir -p ~/.local/bin
 fi
 
 # check if ~/.local/bin/hard exists
 if [ -f ~/.local/bin/hard ]; then
-    rm ~/.local/bin/hard
+  rm ~/.local/bin/hard
 fi
 
 # create a symbolic link to hard.sh
@@ -82,3 +85,6 @@ chmod +x ~/.local/bin/hard
 
 echo "Hard installed successfully!"
 echo "Please, restart your terminal."
+
+~/.local/bin/hard up -d
+~/.local/bin/hard laravel new test
